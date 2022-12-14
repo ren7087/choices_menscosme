7dd0201dfdb957a6cf6aaf608394a9b8fa614033
ren7087 <wakamakiren@icloud.com>
2022-12-14 20:44:56 +0900

    rakutenAPI

--- pages/components/organisms/chartCircle.tsx
+++ pages/components/organisms/chartCircle.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import { supabase } from "../../../utils/supabaseClient";
-import { PostgrestResponse } from "@supabase/supabase-js";
-
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export function ChartCircle() {
---
+++ pages/test.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+// Rakuten APIで用意されているエンドポイント
+const BASE_URL =
+  "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json";
+
+const useFetchData = () => {
+  // エラーを管理するstate
+  const [error, setError] = useState({
+    freeWord: false,
+  });
+  // ローディングする際に使うstate
+  const [fetching, setFetching] = useState(false);
+  // レスポンスを格納するstate
+  const [result, setResult] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        // envは後述
+        `https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?
+	  applicationId=1047547279811101390
+	  &keyword=魚`
+      )
+      .then((response) => {
+        // レスポンスデータを格納
+        setResult(response.data);
+        // ローディング終了
+        setFetching(false);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        setFetching(false);
+      });
+  }, []);
+
+  return <h1>hello</h1>;
+};
+export default useFetchData;
